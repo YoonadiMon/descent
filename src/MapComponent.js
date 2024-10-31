2def2e23f63038d8736cb56b3f9e92c6a256d88c
NCPlays11 <nc11plays@gmail.com>
2024-10-30 22:53:59 -0500

    Map Component

--- src/MapComponent.js
+++ src/MapComponent.js
@@ -1,8 +1,27 @@
-import {} from 'google-map-react';
 const MapComponent = () => {
+    const center = {
+        lat: 32.7502,
+        lng: 114.7655
+      };
+
+      const containerStyle = {
+        width: '400px',
+        height: '400px'
+      };
+      
     return ( 
-        <div className="mapComponent">
-            <h2>Pick a location</h2>
+        <div>
+            <div className="mapComponent">
+                <h2>Pick a location</h2>
+            </div>
+            <iframe
+                width="600"
+                height="450"
+                loading="lazy"
+                allowfullscreen
+                referrerpolicy="no-referrer-when-downgrade"
+                src="https://www.google.com/maps/embed/v1/place?key=AIzaSyACrNXTfikt8TRNFWALOeKF6GQdlrXlgTc&q=United+Kingdom">
+            </iframe>
         </div>
      );
 }
